65cca7a8e5214d927ad314edf3c341bf5b4c7a52
Shockfire <shock@420blaze.it>
2016-01-22 18:20:27 -0500

    Initial source commit

---
+++ ForgeMapFixer/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ForgeMapFixer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.worker = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 48);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(393, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(39, 17);
+            this.statusLabel.Text = "Ready";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(13, 13);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(368, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // worker
+            // 
+            this.worker.WorkerReportsProgress = true;
+            this.worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.worker_DoWork);
+            this.worker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.worker_ProgressChanged);
+            this.worker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.worker_RunWorkerCompleted);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 70);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ElDewrito Forge Map Fixer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.ComponentModel.BackgroundWorker worker;
+    }
+}
+
---
+++ ForgeMapFixer/MainForm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ForgeMapFixer
+{
+    public partial class MainForm : Form
+    {
+        private const string MapDir = @"mods\maps";
+
+        private readonly List<string> _fixedMaps = new List<string>(); 
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (!VerifyInstall())
+            {
+                MessageBox.Show("Please place ForgeMapFixer.exe in the same folder as your ElDewrito 0.5.0.2 install.",
+                    Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+                return;
+            }
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            worker.ReportProgress(0, "Initializing...");
+            var tagMap = LoadTagMap();
+
+            worker.ReportProgress(0, "Searching for map files...");
+            var files = FindMaps();
+
+            _fixedMaps.Clear();
+            for (var i = 0; i < files.Length; i++)
+            {
+                try
+                {
+                    var file = files[i];
+                    worker.ReportProgress(i * 100 / files.Length, "Fixing " + file + "...");
+                    if (FixMap(file, tagMap))
+                        _fixedMaps.Add(file);
+                }
+                catch
+                {
+                    // TODO: Error handling...
+                }
+            }
+
+            worker.ReportProgress(100, "Invalidating cached maps...");
+            InvalidatePreferences();
+
+            worker.ReportProgress(100, "Done!");
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            statusLabel.Text = e.UserState.ToString();
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (_fixedMaps.Count > 0)
+            {
+                var message = $"Successfully fixed {_fixedMaps.Count} map(s):\n";
+                message = _fixedMaps.Aggregate(message, (current, map) => current + $"\n{map}");
+                MessageBox.Show(message, Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No Forge maps seem to be installed.", Text, MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            Close();
+        }
+
+        private static bool VerifyInstall()
+        {
+            if (!File.Exists("eldorado.exe") || !File.Exists("mtndew.dll"))
+                return false;
+
+            // Check that version >= 0.5.0.1
+            var version = FileVersionInfo.GetVersionInfo("mtndew.dll");
+            return version.FileMajorPart > 0 || version.FileMinorPart > 5 ||
+                   (version.FileMinorPart == 5 && (version.FileBuildPart > 0 || version.FilePrivatePart >= 1));
+        }
+
+        private static string[] FindMaps()
+        {
+            return Directory.Exists(MapDir)
+                ? Directory.GetFiles(MapDir, "*.map", SearchOption.AllDirectories)
+                : new string[0];
+        }
+
+        private static Dictionary<int, int> LoadTagMap()
+        {
+            var tagMap = Properties.Resources.ForgeTagMap;
+            var result = new Dictionary<int, int>();
+            using (var reader = new StringReader(tagMap))
+            {
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null)
+                        break;
+                    line = line.Trim();
+                    var parts = line.Split(',');
+                    if (parts.Length < 2)
+                        continue;
+                    int oldIndex, newIndex;
+                    if (!int.TryParse(parts[0].Trim(), NumberStyles.HexNumber, null, out oldIndex))
+                        continue;
+                    if (!int.TryParse(parts[1].Trim(), NumberStyles.HexNumber, null, out newIndex))
+                        continue;
+                    result[oldIndex] = newIndex;
+                }
+            }
+            return result;
+        } 
+
+        private static bool FixMap(string path, IReadOnlyDictionary<int, int> tagMap)
+        {
+            using (var stream = File.Open(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                // Check that the file is valid
+                // I'm just using hardcoded offsets here; this should work for most stuff...
+                if (stream.Length < 0xE1E0)
+                    return false;
+                var reader = new BinaryReader(stream);
+                var writer = new BinaryWriter(stream);
+                if (new string(reader.ReadChars(4)) != "_blf")
+                    return false;
+                stream.Position = 0x138;
+                if (new string(reader.ReadChars(4)) != "mapv")
+                    return false;
+
+                // Go to the tag table and replace all of the indices
+                for (var i = 0; i < 256; i++)
+                {
+                    stream.Position = 0xD498 + i * 0xC;
+                    var tagIndex = reader.ReadInt32();
+                    if (tagIndex < 0)
+                        continue;
+                    int newIndex;
+                    if (!tagMap.TryGetValue(tagIndex, out newIndex))
+                        continue;
+                    stream.Position -= 4;
+                    writer.Write(newIndex);
+                }
+            }
+            return true;
+        }
+
+        private static void InvalidatePreferences()
+        {
+            if (!File.Exists("preferences.dat"))
+                return;
+            using (var writer = new BinaryWriter(File.Open("preferences.dat", FileMode.Open, FileAccess.Write)))
+            {
+                writer.BaseStream.Position = 0x378; // "multiplayer valid" flag
+                writer.Write(0);
+                writer.BaseStream.Position = 0xEA90; // "forge valid" flag
+                writer.Write(0);
+            }
+        }
+    }
+}
